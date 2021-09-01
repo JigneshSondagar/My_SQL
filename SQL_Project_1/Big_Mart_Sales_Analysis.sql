e0b28c870021513a1aa560728c942080ad05d989
Jignesh Sondagar <sondagarjignesh007@gmail.com>
2021-09-01 16:30:13 +0200

    Update Big_Mart_Sales_Analysis.sql

--- SQL_Project_1/Big_Mart_Sales_Analysis.sql
+++ SQL_Project_1/Big_Mart_Sales_Analysis.sql
@@ -4,4 +4,79 @@ CREATE database SQL_Project_1;
 
 use SQL_Project_1;
 
+# Import data 
+
+SELECT * from Big_mart bm ;
+
+# 1. WRITE a sql query to show all Item_Identifier
+
+
+# 2. WRITE a sql query to show count of total Item_Identifier
+
+
+# 3. WRITE a sql query to show maximum Item Weight
+
+
+# 4. WRITE a query to show minimun Item Weight
+
+
+# 5. WRITE a query to show average Item_Weight
+
+
+# 6. WRITE a query to show count OF Item_Fat_Content WHERE Item_Fat_Content IS Low Fat
+
+
+# 7. WRITE a query to show count OF Item_Fat_Content WHERE Item_Fat_Content IS Regular
+
+
+# 8. WRITE a query TO show maximum Item_MRP
+
+
+# 9. WRITE a query TO show minimum Item_MRP
+
+
+# 10. WRITE a query to show Item_Identifier , Item_Fat_Content ,Item_Type,Item_MRP and Item_MRP IS greater than 200
+
+
+# 11. WRITE a query to show maximum Item_MRP WHERE Item_Fat_Content IS Low Fat
+
+
+# 12. WRITE a query to show minimum Item_MRp AND Item_Fat_Content IS Low Fat
+
+
+# 13. WRITE a query to show ALL DATA WHERE item MRP IS BETWEEN 50 TO 100
+
+
+# 14. WRITE a query to show ALL UNIQUE value Item_Fat_Content
+
+
+# 15. WRITE a query to show ALL UNIQUE value Item_Type
+
+
+# 16. WRITE a query to show ALL DATA IN descending ORDER BY Item MRP 
+
+
+# 17. WRITE a query to show ALL DATA IN ascending ORDER BY Item_Outlet_Sales
+
+
+# 18. WRITE a query to show ALL DATA IN ascending BY Item_Type
+
+
+# 19. WRITE a query to show DATA OF item_type dairy & Meat
+
+
+# 20. WRITE a query to show ALL UNIQUE value OF Outlet_Size
+
+
+# 21. WRITE a query to show ALL UNIQUE value OF Outlet_Location_Type 
+
+
+# 22. WRITE a query to show ALL UNIQUE value OF Outlet_Type
+
+
+# 23. WRITE a query to show count NO. OF item BY Item_Type AND ordered it IN descending
+
+
+# 24. 
+ 
 
