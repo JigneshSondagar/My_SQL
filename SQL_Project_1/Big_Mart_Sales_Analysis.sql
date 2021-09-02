9045a36d0f69c3a76e6477291314ca374374fef4
Jignesh Sondagar <sondagarjignesh007@gmail.com>
2021-09-02 09:09:04 +0200

    Update Big_Mart_Sales_Analysis.sql

--- SQL_Project_1/Big_Mart_Sales_Analysis.sql
+++ SQL_Project_1/Big_Mart_Sales_Analysis.sql
@@ -12,7 +12,6 @@ SELECT * from Big_mart bm ;
 SELECT bm.Item_Identifier from Big_mart bm ;
 
 
-
 # 2. WRITE a sql query to show count of total Item_Identifier
 SELECT COUNT(bm.Item_Identifier) as Total_Item_Identifier from Big_mart bm ;
 
@@ -105,6 +104,95 @@ group by Item_Type
 order by No_of_item DESC ;
 
 
-# 24. 
- 
+# 24. WRITE a query to show count NO. OF item BY Outlet_Size AND ordered it IN ascending
+
+
+# 25. WRITE a query to show count NO. OF item BY
+
+
+# 26. WRITE a query to show count NO. OF item BY Outlet_Type AND ordered it IN descending
+
+
+# 27. WRITE a query to show count of item BY Outlet_Location_Type AND ordered it IN descending
+
+
+# 28. WRITE a query to show maximum MRP BY Item_Type
+
+
+# 29. WRITE a query to show minimum MRP BY Item_Type
+
+
+# 30. WRITE a query to show minimum MRP BY Outlet_Establishment_Year AND ordered it IN descending
+
+
+# 31. WRITE a query to show maximum MRP BY Outlet_Establishment_Year AND ordered IN descending
+
+
+# 32. WRITE a query to show average MRP BY Outlet_Size AND ordered IN descending
+
+
+# 33. WRITE a query to show average MRP BY Outlet_Size
+
+
+# 34. WRITE a query to show Average MRP BY Outlet_Type AND ordered IN ascending
+
+
+# 35. WRITE a query to show maximum MRP BY Outlet_Type 
+
+
+# 36. WRITE a query to show maximum Item_Weight BY Item_Type 
+
+
+# 37. WRITE a query to show maximum Item_Weight BY Outlet_Establishment_Year
+
+
+# 38. WRITE a query to show minimum Item_Weight BY Outlet_Type 39.WRITE a query to show average Item_Weight BY Outlet_Location_Type ORDER BY descending
+
+
+# 40. WRITE a query to show maximum Item_Outlet_Sales BY Item_Type 
+
+
+# 41.WRITE a query to show minimum Item_Outlet_Sales BY Item_Type 
+
+
+# 42.WRITE a query to show minimum Item_Outlet_Sales BY Outlet_Establishment_Year
+
+
+# 43.WRITE a query to show maximum Item_Outlet_Sales BY Outlet_Establishment_Year ordered BY descending
+
+
+# 44.WRITE a query to show average Item_Outlet_Sales BY Outlet_Size AND ORDER it itn descending
+
+
+# 45.WRITE a query to show average Item_Outlet_Sales BY Outlet_Size 
+
+
+# 46.WRITE a query to show average Item_Outlet_Sales BY Outlet_Type 
+
+
+# 47.WRITE a query to show maximum Item_Outlet_Sales BY Outlet_Type 
+
+
+# 48.WRITE a query to show total Item_Outlet_Sales BY
+
+
+# 49.WRITE a query to show total Item_Outlet_Sales BY Item_Type 
+
+
+# 50.WRITE a query to show total Item_Outlet_Sales BY
+
+
+# 51.WRITE a query to show total Item_Outlet_Sales BY Item_Fat_Content 
+
+
+# 52.WRITE a query to show maximum Item_Visibility BY Item_Type 
+
+
+# 53.WRITE a query to show Minimum Item_Visibility BY Item_Type 
+
+
+# 54.WRITE a query to show total Item_Outlet_Sales BY Item_Type but ONLY WHERE Outlet_Location_Type IS Tier 1
+
+
+# 55.WRITE a query to show total Item_Outlet_Sales BY Item_Type WHERE Item_Fat_Content IS ONLY Low Fat & LF
 
